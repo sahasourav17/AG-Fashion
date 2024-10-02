9157dd327f11029bc810fceafc4289f247e287c7
sahasourav17 <sahasourav1170@gmail.com>
2024-10-02 11:46:57 +0600

    init and add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Anek_Bangla, Inter } from "next/font/google";
 import "./globals.css";
+import { Footer } from "@/components/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const anek = Anek_Bangla({
+  subsets: ["bengali"],
+  variable: "--font-anek-bangla",
 });
 
 export const metadata: Metadata = {
@@ -24,11 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" data-theme="light">
+      <body className={`${inter.className} ${inter.variable}`}>
+        <main className={anek.variable}>
+          {children}
+          <Footer />
+        </main>
       </body>
     </html>
   );
